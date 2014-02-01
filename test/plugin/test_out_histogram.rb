6611cb3ea143002aaf01a831ed4d8e60837cdb4d
Yusuke SHIMIZU <a.ryuklnm@gmail.com>
2014-02-01 16:22:51 +0900

    refactoring test code

--- test/plugin/test_out_histogram.rb
+++ test/plugin/test_out_histogram.rb
@@ -31,38 +31,38 @@ class HistogramOutputTest < Test::Unit::TestCase
   def test_small_increment_no_alpha
     bin_num = 100
     alpha = 0
-    f = create_driver(%[ 
+    f = create_driver(%[
                         bin_num #{bin_num}
                         alpha #{alpha}])
     f.instance.increment("test.input", "A")
     f.instance.increment("test.input", "B")
-    zero = f.instance.zero_hist.dup
+    hist = f.instance.zero_hist.dup
     id = "A".hash % bin_num
-    zero[id] += 1
+    hist[id] += 1
     id = "B".hash % bin_num
-    zero[id] += 1
-    assert_equal({"test.input" => {:hist => zero, :sum => 2, :avg => 2/bin_num, :sd=>0}}, 
+    hist[id] += 1
+    assert_equal({"test.input" => {:hist => hist, :sum => 2, :avg => 2/bin_num, :sd=>0}},
                  f.instance.flush)
   end
 
   def test_small_increment_with_alpha
     bin_num = 100
     alpha = 1
-    f = create_driver(%[ 
+    f = create_driver(%[
                         bin_num #{bin_num}
                         alpha #{alpha}])
     f.instance.increment("test.input", "A")
     f.instance.increment("test.input", "B")
-    zero = f.instance.zero_hist.dup
+    hist = f.instance.zero_hist.dup
     id = "A".hash % bin_num
-    zero[id] += 2
-    zero[(id + alpha) % bin_num] += 1
-    zero[id - alpha] += 1
+    hist[id] += 2
+    hist[(id + alpha) % bin_num] += 1
+    hist[id - alpha] += 1
     id = "B".hash % bin_num
-    zero[id] += 2
-    zero[(id + alpha) % bin_num] += 1
-    zero[id - alpha] += 1
-    assert_equal({"test.input" => {:hist => zero, :sum => 2*3+2, :avg => (2*3+2)/bin_num, :sd=>0}}, 
+    hist[id] += 2
+    hist[(id + alpha) % bin_num] += 1
+    hist[id - alpha] += 1
+    assert_equal({"test.input" => {:hist => hist, :sum => 2, :avg => 2/bin_num, :sd=>0}},
                  f.instance.flush)
   end
 
@@ -108,30 +108,30 @@ class HistogramOutputTest < Test::Unit::TestCase
 
   def test_increment_sum
     bin_num = 100
-    f = create_driver(%[ 
+    f = create_driver(%[
                         bin_num #{bin_num}
                         alpha   1 ])
     1000.times do |i|
       f.instance.increment("test.input", i.to_s)
     end
     flushed = f.instance.flush
-    assert_equal(1000*4, flushed["test.input"][:sum])
-    assert_equal(1000*4/bin_num, flushed["test.input"][:avg])
+    assert_equal(1000, flushed["test.input"][:sum])
+    assert_equal(1000/bin_num, flushed["test.input"][:avg])
   end
 
   def test_emit
     bin_num = 100
-    f = create_driver(%[ 
+    f = create_driver(%[
                       bin_num #{bin_num}
                       alpha 1 ])
     f.run do
-      100.times do 
+      100.times do
         f.emit({"keys" => ["A", "B", "C"]})
       end
     end
     flushed = f.instance.flush
-    assert_equal(300*4, flushed["test"][:sum])
-    assert_equal(300*4/bin_num, flushed["test"][:avg])
+    assert_equal(300, flushed["test"][:sum])
+    assert_equal(300/bin_num, flushed["test"][:avg])
   end
 
   def test_some_hist_exist_case_tagging_with_emit
@@ -162,20 +162,20 @@ class HistogramOutputTest < Test::Unit::TestCase
                         input_tag_remove_prefix test])
     # ("A".."ZZ").to_a.size == 702
     data = ("A".."ZZ").to_a.shuffle
-    f.run do 
-      100.times do 
+    f.run do
+      100.times do
         data.each_slice(10) do |d|
           f.emit({"keys" => d})
         end
       end
     end
     flushed_even = f.instance.flush
-    
+
     #('A'..'ZZ').to_a.shuffle.size == 702
     # In here, replace 7 values of ('A'..'ZZ') to 'D' as example hotspot.
     data.size.times {|i| data[i] = 'D' if i%100 == 0 }
-    f.run do 
-      100.times do 
+    f.run do
+      100.times do
         data.each_slice(10) do |d|
           f.emit({"keys" =>  d})
         end
@@ -183,9 +183,9 @@ class HistogramOutputTest < Test::Unit::TestCase
     end
     flushed_bias = f.instance.flush
 
-    assert_equal(true, flushed_even["histo.localhost"][:sd] < flushed_bias["histo.localhost"][:sd], 
-                 "expected 
-even:#{flushed_even["histo.localhost"]} 
+    assert_equal(true, flushed_even["histo.localhost"][:sd] < flushed_bias["histo.localhost"][:sd],
+                 "expected
+even:#{flushed_even["histo.localhost"]}
  <
 bias:#{flushed_bias["histo.localhost"]}")
   end
@@ -193,18 +193,18 @@ bias:#{flushed_bias["histo.localhost"]}")
   def test_sampling
     bin_num = 100
     sampling_rate = 10
-    f = create_driver(%[ 
+    f = create_driver(%[
                       bin_num       #{bin_num}
                       sampling_rate #{sampling_rate}
                       alpha 0 ])
     f.run do
-      sampling_rate.times do 
+      sampling_rate.times do
         f.emit({"keys" => ["A"]})
       end
     end
     flushed = f.instance.flush
     assert_equal(sampling_rate, flushed["test"][:sum])
- 
+
     f.run do
       1.times do  # 1 < sampling_rate
         f.emit({"keys" => ["A"]})
@@ -214,7 +214,7 @@ bias:#{flushed_bias["histo.localhost"]}")
     assert_equal(0, flushed["test"][:sum])
 
     f.run do
-      100.times do 
+      100.times do
         f.emit({"keys" => ["A", "B", "C"]})
       end
     end
